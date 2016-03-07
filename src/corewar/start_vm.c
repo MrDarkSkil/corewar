80b880b23115f666835fc3c18e85bfc431b23f03
descho_e <eric.deschodt@epitech.eu>
2016-03-07 14:00:57 +0100

    chips

--- src/corewar/start_vm.c
+++ src/corewar/start_vm.c
@@ -5,36 +5,43 @@
 ** Login   <descho_e@epitech.net>
 ** 
 ** Started on  Mon Mar  7 13:35:33 2016 Eric DESCHODT
-** Last update Mon Mar  7 13:44:23 2016 Eric DESCHODT
+** Last update Mon Mar  7 14:00:44 2016 Eric DESCHODT
 */
 
 #include "corewar.h"
 
-void		exec_champ(t_champ *champ)
+void		exec_champ(t_champlist *champ)
 {
   t_instru	*cur_instru;
 
-  cur_instru = champ->list;
+  cur_instru = champ->begin;
   if (cur_instru.current_cycle == 0)
+    printf("%d : %s\n", champ->id, cur_instru.ope.mnemonique);
+  cur_instru.current_cycle++;
+  if (cur_instru.current_cycle == cur_instru.ope.nbr_cycles)
     {
-      my_putstr(cu
+      cur_instru.current_cycle = 0;
+      champ->begin = champ->begin->next;
     }
 }
 
 int		start_vm(t_vm *vm)
 {
-  t_champ	*cur_champ;
+  t_champlist	*cur_champ;
+  int		cycle;
   int		i;
 
-  while (42)
+  cycle = CYCLE_TO_DIE;
+  while (cycle > 0)
     {
       i = 0;
-      cur_champ = vm->list;
+      cur_champ = vm->begin;
       while (i < 4)
 	{
 	  exec_champ(cur_champ);
 	  cur_champ = cur_champ->next;
 	  i++;
 	}
+      cycle -= CYCLE_DELTA;
     }
 }
