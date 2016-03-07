c127a19d3a00bdea2e6389444f02d248f4ce8db5
descho_e <eric.deschodt@epitech.eu>
2016-03-07 13:31:25 +0100

    [*] ajout d'une variable

--- include/corewar.h
+++ include/corewar.h
@@ -5,7 +5,7 @@
 ** Login   <descho_e@epitech.net>
 ** 
 ** Started on  Mon Mar  7 12:59:17 2016 Eric DESCHODT
-** Last update Mon Mar  7 13:17:35 2016 Eric DESCHODT
+** Last update Mon Mar  7 13:31:08 2016 Eric DESCHODT
 */
 
 #ifndef COREWAR_H_
@@ -16,6 +16,7 @@
 typedef struct		s_instru
 {
   op_t			ope;
+  int			current_cycle;
   struct s_instru	*next;
 }			t_instru;
 
