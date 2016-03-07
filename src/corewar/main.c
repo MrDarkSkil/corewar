8e8877565b939585570a8fa00f29cf52024a9126
Antoine Roig <antoine.roig@epitech.eu>
2016-03-07 15:55:42 +0100

    norme

--- src/corewar/main.c
+++ src/corewar/main.c
@@ -5,91 +5,10 @@
 ** Login   <descho_e@epitech.net>
 ** 
 ** Started on  Mon Mar  7 13:26:06 2016 Eric DESCHODT
-** Last update Mon Mar  7 15:38:21 2016 Antoine Roig
+** Last update Mon Mar  7 15:55:03 2016 Antoine Roig
 */
 
 #include "corewar.h"
-
-void    *xmalloc(int size)
-{
-  void  *element;
-
-  element = malloc(size);
-  if (!element)
-    exit(EXIT_FAILURE);
-  return (element);
-}
-
-t_vm	*new_vm(t_vm *vm)
-{
-  vm = xmalloc(sizeof(*vm));
-  vm->begin = NULL;
-  vm->end = vm->begin;
-  vm->nb = 0;
-  return (vm);
-}
-
-t_champlist	*new_champ(t_champlist *champ, int id, int cycle)
-{
-  champ = xmalloc(sizeof(*champ));
-  champ->end = champ->begin;
-  champ->id = id;
-  champ->next = NULL;
-  champ->begin = malloc(sizeof(t_instru));
-  champ->current = champ->begin;
-  champ->begin->ope.comment = "yo";
-  champ->begin->ope.nbr_cycles = cycle;
-  champ->begin->current_cycle = 0;
-  champ->begin->next = champ->begin;
-  return (champ);
-}
-
-void	add_champ_vm(t_vm *vm, t_champlist *champ, int nb_champ, int cycle)
-{
-  champ = new_champ(champ, nb_champ, cycle);
-  if (vm->begin == NULL)
-    {
-      vm->begin = champ;
-      vm->end = champ;
-      vm->nb = 1;
-    }
-  else
-    {
-      champ->next = vm->begin;
-      vm->begin->prev = champ;
-      vm->begin = champ;
-      vm->nb = vm->nb + 1;
-    }
-}
-
-void	launch_vm(t_vm *vm, int nb_champ)
-{
-  int		i;
-  t_champlist	*champ;
-  int		cycle;
-
-  cycle = 2;
-  i = 0;
-  while (i < nb_champ)
-    {
-      champ = NULL;
-      add_champ_vm(vm, champ, i, cycle);
-      cycle *= 2;
-      i++;
-    }
-}
-
-void    show_list(t_vm *vm)
-{
-  t_champlist        *tmp;
-
-  tmp = vm->begin;
-  while (tmp != NULL)
-    {
-      printf("id = %d\ncomment = %s\nnb cycle = %d", (tmp->id), tmp->begin->ope.comment, tmp->begin->ope.nbr_cycles);
-      tmp = tmp->next;
-    }
-}
 
 int	main(int ac, char **av)
 {
