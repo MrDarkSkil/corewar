4cc2daf3863c32613dbba44557b9e4b0a3a45fd6
Antoine Roig <antoine.roig@epitech.eu>
2016-03-07 16:02:44 +0100

    vm ma bite

--- src/corewar/main.c
+++ src/corewar/main.c
@@ -5,95 +5,11 @@
 ** Login   <descho_e@epitech.net>
 ** 
 ** Started on  Mon Mar  7 13:26:06 2016 Eric DESCHODT
-** Last update Mon Mar  7 15:59:36 2016 Antoine Roig
+** Last update Mon Mar  7 16:02:26 2016 Antoine Roig
 */
 
 #include "corewar.h"
 
-void    *xmalloc(int size)
-{
-  void  *element;
-
-  element = malloc(size);
-  if (!element)
-    exit(EXIT_FAILURE);
-  return (element);
-}
-
-t_vm	*new_vm(t_vm *vm)
-{
-  vm = xmalloc(sizeof(*vm));
-  vm->begin = NULL;
-  vm->end = vm->begin;
-  vm->nb = 0;
-  return (vm);
-}
-
-t_champlist	*new_champ(t_champlist *champ, int id, int cycle)
-{
-  champ = xmalloc(sizeof(*champ));
-  champ->end = champ->begin;
-  champ->id = id;
-  champ->next = NULL;
-  champ->begin = malloc(sizeof(t_instru));
-  champ->current = champ->begin;
-  champ->begin->ope.comment = "yo";
-  champ->begin->ope.nbr_cycles = cycle;
-  champ->begin->current_cycle = 0;
-  champ->begin->next = champ->begin;
-  return (champ);
-}
-
-void	add_champ_vm(t_vm *vm, t_champlist *champ, int nb_champ, int cycle)
-{
-  champ = new_champ(champ, nb_champ, cycle);
-  if (vm->begin == NULL)
-    {
-      vm->begin = champ;
-      vm->end = champ;
-      vm->nb = 1;
-    }
-  else
-    {
-      champ->next = vm->begin;
-      vm->begin->prev = champ;
-      vm->begin = champ;
-      vm->nb = vm->nb + 1;
-    }
-}
-
-void	launch_vm(t_vm *vm, int nb_champ)
-{
-  int		i;
-  t_champlist	*champ;
-  int		cycle;
-
-  cycle = 2;
-  i = 0;
-  while (i < nb_champ)
-    {
-      champ = NULL;
-      add_champ_vm(vm, champ, i, cycle);
-      cycle *= 2;
-      i++;
-    }
-}
-
-void    show_list(t_vm *vm)
-{
-  t_champlist        *tmp;
-
-  tmp = vm->begin;
-  while (tmp != NULL)
-    {
-      my_printf("id = %d\ncomment = %s\nnb cycle = %d\n",
-		(tmp->id), tmp->begin->ope.comment, tmp->begin->ope.nbr_cycles);
-      tmp = tmp->next;
-    }
-
-}
-
->>>>>>> d932ef16e4c3e02158ac3b4132ef235cb26888fd
 int	main(int ac, char **av)
 {
   int	nb_champ;
@@ -104,9 +20,6 @@ int	main(int ac, char **av)
   nb_champ = atoi(av[1]);
   vm = new_vm(vm);
   launch_vm(vm, nb_champ);
-<<<<<<< HEAD
-=======
   start_vm(vm);
->>>>>>> d932ef16e4c3e02158ac3b4132ef235cb26888fd
   return (0);
 }
