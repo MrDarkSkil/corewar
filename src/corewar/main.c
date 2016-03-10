68474322951395e10228950f6f7657b4a4fd0019
descho_e <eric.deschodt@epitech.eu>
2016-03-10 17:11:01 +0100

    chips

--- src/corewar/main.c
+++ src/corewar/main.c
@@ -5,7 +5,7 @@
 ** Login   <descho_e@epitech.net>
 ** 
 ** Started on  Mon Mar  7 13:26:06 2016 Eric DESCHODT
-** Last update Thu Mar 10 15:19:26 2016 Eric DESCHODT
+** Last update Thu Mar 10 17:09:46 2016 Eric DESCHODT
 */
 
 #include "corewar.h"
@@ -33,23 +33,57 @@ void		print_info(header_t *head)
   my_printf("%s\n", head->comment);
 }
 
+void		init_board(char *board)
+{
+  int		i;
+
+  i = 0;
+  while (i < MEM_SIZE)
+    {
+      board[i] = 0;
+      i++;
+    }
+}
+
+void		printboard(char *board)
+{
+  int		i;
+
+  i = 0;
+  while (i < MEM_SIZE)
+    printf("%x ", board[i++]);
+  printf("\n");
+}
+
 int		main(int ac, char **av)
 {
   int		fd;
   header_t	head;
+  char		*prog;
+  char		board[MEM_SIZE];
+  int		i;
+  int		a;
 
-  if ((ac == 0)
-      || (fd = open(av[1], O_RDONLY)) == -1)
+  if ((ac <= 2)
+      || (fd = open(av[3], O_RDONLY)) == -1)
     return (0);
-  read(fd, &head, PROG_NAME_LENGTH + COMMENT_LENGTH + 4 + 4); 
+  init_board(board);
+  read(fd, &head, PROG_NAME_LENGTH + COMMENT_LENGTH + (4 + 4) * 2); 
   revert_endian(&head.magic);
   revert_endian(&head.prog_size);
-  print_info(&head);
   if (head.magic != COREWAR_EXEC_MAGIC)
     {
       my_printf("Invalid Magic Number\n");
       return (-1);
     }
-  close (fd);
+  if (((prog = malloc(sizeof(char) * head.prog_size + 1)) == NULL)
+      || read(fd, prog, head.prog_size) == -1)
+    return (-1);
+  a = my_getnbr(av[2]) % MEM_SIZE;
+  i = 0;
+  while (i < head.prog_size)
+    board[a++] = prog[i++];
+  printboard(board);
+   close (fd);
   return (0);
 }
