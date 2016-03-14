57e5b32224e6f2e4a6eb2ef9f3f78c0e08bb8a54
descho_e <eric.deschodt@epitech.eu>
2016-03-14 09:16:30 +0100

    chips

--- include/corewar.h
+++ include/corewar.h
@@ -5,11 +5,8 @@
 ** Login   <descho_e@epitech.net>
 ** 
 ** Started on  Mon Mar  7 12:59:17 2016 Eric DESCHODT
-<<<<<<< HEAD
-** Last update Fri Mar 11 12:36:31 2016 Juliani Renaud
-=======
-** Last update Tue Mar  8 17:30:26 2016 Eric DESCHODT
->>>>>>> 4d69422aaf278a3bd23f86782f2c61c86cc97175
+** Last update Mon Mar 14 09:13:04 2016 Eric DESCHODT
+
 */
 
 #ifndef COREWAR_H_
@@ -27,41 +24,32 @@
 # define MAGIC_NUMBER 85
 
 
-typedef struct		s_instru
+typedef struct		s_champ
 {
-  op_t			ope;
-  int			current_cycle;
-  struct s_instru	*next;
-}			t_instru;
-
-typedef struct		s_champlist
-{
-  t_instru		*begin;
-  t_instru		*end;
-  t_instru		*current;
   int			id;
-  struct s_champlist	*next;
-  struct s_champlist	*prev;
-}			t_champlist;
+  unsigned char		*instru;
+  unsigned int		cycle;
+  int			size;
+  struct s_champ	*next;
+  struct s_champ	*prev;
+}			t_champ;
 
 typedef struct		s_vm
 {
-  t_champlist		*begin;
-  t_champlist		*end;
+  t_champ		*begin;
+  t_champ		*end;
   int			nb;
 }			t_vm;
 
-/* <<<<<<< HEAD */
 void	my_puterror(char *str);
 char	*my_memeset(char *str, char c, int len);
-/* ======= */
+
 typedef union		u_byte
 {
   char			byte[4];
   unsigned int		full;
 }			t_byte;
 
-/* >>>>>>> 4d69422aaf278a3bd23f86782f2c61c86cc97175 */
 void    *xmalloc(int size);
 t_vm	*new_vm(t_vm *vm);
 void	launch_vm(t_vm *vm, int nb_champ);
--- src/corewar/main.c
+++ src/corewar/main.c
@@ -5,7 +5,7 @@
 ** Login   <descho_e@epitech.net>
 ** 
 ** Started on  Mon Mar  7 13:26:06 2016 Eric DESCHODT
-** Last update Thu Mar 10 17:09:46 2016 Eric DESCHODT
+** Last update Mon Mar 14 09:13:16 2016 Eric DESCHODT
 */
 
 #include "corewar.h"
@@ -33,7 +33,7 @@ void		print_info(header_t *head)
   my_printf("%s\n", head->comment);
 }
 
-void		init_board(char *board)
+void		init_board(unsigned char *board)
 {
   int		i;
 
@@ -45,45 +45,66 @@ void		init_board(char *board)
     }
 }
 
-void		printboard(char *board)
+void		printboard(unsigned char *board)
 {
   int		i;
 
   i = 0;
   while (i < MEM_SIZE)
-    printf("%x ", board[i++]);
+    {
+      my_printf("%s%x ", (board[i] < 16) ? "0" : "",  board[i]);
+      i++;
+    }
   printf("\n");
 }
 
-int		main(int ac, char **av)
+int		get_header(int fd, header_t *head)
 {
-  int		fd;
-  header_t	head;
-  char		*prog;
-  char		board[MEM_SIZE];
-  int		i;
-  int		a;
-
-  if ((ac <= 2)
-      || (fd = open(av[3], O_RDONLY)) == -1)
-    return (0);
-  init_board(board);
-  read(fd, &head, PROG_NAME_LENGTH + COMMENT_LENGTH + (4 + 4) * 2); 
-  revert_endian(&head.magic);
-  revert_endian(&head.prog_size);
-  if (head.magic != COREWAR_EXEC_MAGIC)
+  read(fd, head, PROG_NAME_LENGTH + COMMENT_LENGTH + (4 + 4) * 2); 
+  revert_endian(&head->magic);
+  revert_endian(&head->prog_size);
+  if (head->magic != COREWAR_EXEC_MAGIC)
     {
       my_printf("Invalid Magic Number\n");
       return (-1);
     }
-  if (((prog = malloc(sizeof(char) * head.prog_size + 1)) == NULL)
+  return (0);
+}
+
+int		create_champ(t_champ *new_elem,
+			     char *name,
+			     unsigned int a,
+			     unsigned char *board)
+{
+  int		fd;
+  char		*prog;
+  int		i;
+  header_t	head;
+
+  if ((fd = open(name, O_RDONLY)) == -1
+      || get_header(fd, &head) == -1
+      || (prog = malloc(sizeof(char) * head.prog_size + 1)) == NULL
       || read(fd, prog, head.prog_size) == -1)
     return (-1);
-  a = my_getnbr(av[2]) % MEM_SIZE;
+  new_elem->instru = &board[a];
+  new_elem->cycle = 0;
+  new_elem->size = head.prog_size;
   i = 0;
   while (i < head.prog_size)
     board[a++] = prog[i++];
+  close (fd);
+  free(prog);
+  return (0);
+}
+int		main(int ac, char **av)
+{
+  unsigned char	board[MEM_SIZE];
+  t_champ	new_elem;
+
+  if (ac < 2)
+    return (0);
+  init_board(board);
+  create_champ(&new_elem, av[1], 0, board);
   printboard(board);
-   close (fd);
   return (0);
 }
