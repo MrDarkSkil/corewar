e148906285550a20daed95a3e68b6c86f8ec413d
Juliani Renaud <julian_r@epitech.eu>
2016-03-14 17:19:10 +0100

    message auto, oui oui

--- include/corewar.h
+++ include/corewar.h
@@ -6,7 +6,7 @@
 ** 
 ** Started on  Mon Mar  7 12:59:17 2016 Eric DESCHODT
 <<<<<<< HEAD
-** Last update Fri Mar 11 12:36:31 2016 Juliani Renaud
+** Last update Mon Mar 14 17:05:34 2016 Juliani Renaud
 =======
 ** Last update Tue Mar  8 17:30:26 2016 Eric DESCHODT
 >>>>>>> 4d69422aaf278a3bd23f86782f2c61c86cc97175
@@ -16,6 +16,7 @@
 # define COREWAR_H_
 
 # include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/types.h>
@@ -33,6 +34,13 @@ typedef struct		s_instru
   int			current_cycle;
   struct s_instru	*next;
 }			t_instru;
+
+typedef struct		s_headers
+{
+  int			magic;
+  char			*name;
+  char			*comment;
+}			t_headers;
 
 typedef struct		s_champlist
 {
--- src/asm/main.c
+++ src/asm/main.c
@@ -5,7 +5,7 @@
 ** Login   <julian_r@epitech.net>
 ** 
 ** Started on  Fri Mar 11 11:03:06 2016 Juliani Renaud
-** Last update Fri Mar 11 12:34:00 2016 Juliani Renaud
+** Last update Mon Mar 14 17:19:03 2016 Juliani Renaud
 */
 
 #include "corewar.h"
@@ -34,6 +34,17 @@ char		*my_memeset(char *str, char c, int len)
   while (i < len)
     str[i++] = c;
   return (str);
+}
+
+int		swap(int magic)
+{
+  int		new_magic;
+
+  printf("1 : %d\n", magic);
+  new_magic = ((magic >> 24) &0xff) | ((magic << 8) &0xff0000)
+    | ((magic >> 8) &0xff00) | ((magic << 24) &0xff000000);
+  printf("2 : %d\n", new_magic);
+  return (new_magic);
 }
 
 char		*recup(char *str)
@@ -76,20 +87,17 @@ char		*changopen(char *str)
   return (rturn);
 }
 
-void		write_name_comment(int fdwrite, char *name, char *comment)
+void		write_name_comment(int fdwrite, t_headers *head)
 {
-  if ((write(fdwrite, name, PROG_NAME_LENGTH)) == -1)
+  if ((write(fdwrite, head, PROG_NAME_LENGTH)) == -1)
     my_puterror("Error with write name in write_name_comment\n");
-  if ((write(fdwrite, comment, COMMENT_LENGTH)) == -1)
-    my_puterror("Error with write comment in write_name_comment\n");
 }
 
 int		main(int ac, char **av)
 {
   int		fdread;
   int		fdwrite;
-  char		*name;
-  char		*comment;
+  t_headers	*head;
 
   ac = ac;
   if ((fdread = open(av[1], O_RDONLY)) == -1)
@@ -97,14 +105,17 @@ int		main(int ac, char **av)
   if ((fdwrite = open(changopen(av[1]), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 		      | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)) == -1)
     my_puterror("Error with open av in main\n");
-  if ((name = malloc(sizeof(char) * (PROG_NAME_LENGTH + 1))) == NULL)
+  if ((head = malloc(sizeof(head))) == NULL)
+    my_puterror("Error with malloc head in main\n");
+  if ((head->name = malloc(sizeof(char) * (PROG_NAME_LENGTH + 1))) == NULL)
     my_puterror("Error with malloc name in main\n");
-  if ((comment = malloc(sizeof(char) * (COMMENT_LENGTH + 1))) == NULL)
+  if ((head->comment = malloc(sizeof(char) * (COMMENT_LENGTH + 1))) == NULL)
     my_puterror("Error with malloc name in main\n");
-  name = my_memeset(name, '\0', (PROG_NAME_LENGTH + 1));
-  comment = my_memeset(comment, '\0', (COMMENT_LENGTH + 1));
-  name = recup(get_next_line(fdread));
-  comment = recup(get_next_line(fdread));
-  write_name_comment(fdwrite, name, comment);
+  head->magic = swap(COREWAR_EXEC_MAGIC);
+  head->name = my_memeset(head->name, '\0', (PROG_NAME_LENGTH + 1));
+  head->comment = my_memeset(head->comment, '\0', (COMMENT_LENGTH + 1));
+  head->name = recup(get_next_line(fdread));
+  head->comment = recup(get_next_line(fdread));
+  write_name_comment(fdwrite, head);
   return (0);
 }
