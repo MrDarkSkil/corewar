ec56e37529ad4addbfd3d4870b1110439a68387f
descho_e <eric.deschodt@epitech.eu>
2016-03-25 22:37:13 +0100

    bonus

---
+++ bonus/include/igraph.h
@@ -0,0 +1,24 @@
+/*
+** libtekgui.h for  in /home/descho_e/year_2015_2016/I_graph/gfx_tekgui
+** 
+** Made by Eric DESCHODT
+** Login   <descho_e@epitech.net>
+** 
+** Started on  Tue Feb  2 13:19:12 2016 Eric DESCHODT
+** Last update Mon Feb 29 09:22:42 2016 Eric DESCHODT
+*/
+
+#ifndef			IGRAPH_H_
+# define		IGRAPH_H_
+
+# include <unistd.h>
+# include "lapin.h"
+
+typedef struct		s_data
+{
+  t_bunny_window	*win;
+  t_bunny_pixelarray	*pix;
+}			t_data;
+
+# endif /* IGRAPH_H_ */
+
